c2d45a51c5e3664ae3c762d3e005792cb86f9c1e
gamberfox <gamberfox@gmail.com>
2024-10-10 09:52:22 -0500

    prepping for development

--- stock-api-service/src/main/java/com/emazon/stock_api_service/infrastructure/exception/CategoryPersistenceException.java
+++
@@ -1,7 +0,0 @@
-package com.emazon.stock_api_service.infrastructure.exception;
-
-public class CategoryPersistenceException extends RuntimeException {
-    public CategoryPersistenceException(String message) {
-        super(message);
-    }
-}
--- stock-api-service/src/test/java/com/emazon/stock_api_service/StockApiServiceApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package com.emazon.stock_api_service;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StockApiServiceApplicationTests {
-}
